4acf2012f37eb04580a560bf69ee794b253fe6c1
edouard06 <edurand.viel@gmail.com>
2018-02-05 22:07:19 +0100

    nouvelle version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { myText } from './Content';
-import { ContentService } from './content.services';
-
-import { Http , Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+// Component decorators
+import { Component } from '@angular/core';
+// From http injectable get Map operator
 import 'rxjs/add/operator/map';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ContentService]
+// Decorator for MetaData invoquing Components Edouard
+@Component ({
+   selector: 'demo-app',
+   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
-  title = 'Edouard. New Angular 4 App';
+  name: string = 'Edouard Durand-Viel CV-Tech';
+  appStatus: boolean = true;
 
-  secondTitle = 'second title for ng4';
-
-  date = new Date();
-
-
-  findDay = function(){
-    if (this.date.getDate() < 10) {
-       var day: String = '0' + this.date.getDate();
-    } else {
-       var day: String = this.date.getDate();
-    }
-    return day;
-  };
-
-  findMonth = function(){
-      if ((this.date.getMonth() + 1 ) < 10) {
-        var month: String = '0' + (this.date.getMonth() + 1 );
-       } else {
-         var month: String = this.date.getMonth() + 1;
-      }
-      return month;
-    };
-
-  currentDay = this.findDay();
-
-  currentMonth = this.findMonth();
-
-  subtitle = 'The current Date: '+ this.currentDay + '/'+ this.currentMonth + '/'+ this.date.getFullYear();
-
-
-   itext : myText[];
-   constructor(private _text: ContentService) {}
-   getTexts(): void {
-    this._text.getTexts().then(itext => this.itext = itext);
-  }
-   ngOnInit(): void {
-    console.log(this.getTexts() + ' 1');
-   }
 }
