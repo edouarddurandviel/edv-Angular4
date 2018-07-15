1846b9c7bb8c3d8ed2ec76d775a76f779d308d9f
edouarddurandviel <edurand.viel@gmail.com>
2018-07-15 16:20:27 +0200

    Dernière version edv

--- trains.js
+++ trains.js
@@ -1,6 +1,4 @@
-(function (){
-
-    var trains =  [
+  var trains =  [
         {
             passagers: 10,
             stations: 6,
@@ -19,52 +17,65 @@
     ];  
         
 
-     // check if all the conditions are completed including if is it an interconnection ?
+    
     var canTurn = function(train_id){
-        var train = train_id;
-
-            var j = -1;
-            while(j){
-                    if(train.node[j] === 0){ // if the node value is positive set turn function to TRUE and is autorized to turn
-                         return false;
-                    }else if(train.node[j] === 1 && j >= train.stations){
-                        return true;
-                    }
-                j++;
+            for(var j = -1; j < train[train_id].node.length; j++){ 
+                if(train[train_id].node[j] === 0){ 
+                    return false;
+                }else if(train[train_id].node[j] === 1){
+                    return true;
+                }
             }
-        
+        }
+
+    var enoughStations = function(nb){
+        var n = 0;
+        while(n){
+            if(n >= trains[nb].stations){
+                return true;
+            }else{
+                return false;
+            }
+            n++;
+        }
+    }
+
+    var combine = function(trains){
+        for(var i = -1; i < trains.length; i++){ 
+            if(enoughStations(i) === true && canTurn(i) === true){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 
 
     var canGo = function(){
 
         switch(trains) {
+            
             case trains[0].passagers > trains[1].passagers || trains[0].passagers > trains[2].passagers :
                 return {
-                    lets_turn: canTurn(trains[0]), // return true or false
+                    lets_turn: combine(trains[0]), // return true or false
                 } 
             
             case trains[1].passagers > trains[0].passagers || trains[1].passagers > trains[2].passagers :
                 return {
-                    lets_turn: canTurn(trains[1]), // return true or false
+                    lets_turn: combine(trains[1]), // return true or false
                 }
             
             case trains[2].passagers > trains[0].passagers || trains[2].passagers > trains[1].passagers :
                 return {
-                    lets_turn: canTurn(trains[2]), // return true or false
+                    lets_turn: combine(trains[2]), // return true or false
                 }
         } 
         
-        console.log(trains[0].passagers);
         
     }
 
     setTimeout(canGo(), 1000);
  
-
-})();
-
-
 
 
 
